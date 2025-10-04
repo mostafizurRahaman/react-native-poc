9e7393ddce33f256833842952968372aa4cdb6aa
mostafizurRahaman <mostafizurrahaman0401@gmail.com>
2025-10-04 21:49:05 +0600

    feat: view components

--- App.tsx
+++ App.tsx
@@ -1,45 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import ViewComponents from './src/components/ViewComponent';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+const App = () => {
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+      <SafeAreaView>
+        <ViewComponents />
+      </SafeAreaView>
     </SafeAreaProvider>
   );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+};
 
 export default App;
---
+++ src/components/ViewComponent.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const ViewComponents = () => {
+  return (
+    <View>
+      <Text>View Component + Text Components</Text>
+    </View>
+  );
+};
+
+export default ViewComponents;
