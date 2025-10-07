addd7981ab2178c04cf3bf17060437a99de28ed9
mostafizurRahaman <mostafizurrahaman0401@gmail.com>
2025-10-08 00:16:47 +0600

    fix: Learned Pressable component in React Native

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,4 +2,5 @@ export * from './ButtonComponent';
 export * from './ViewComponent';
 export * from './StyleSheet';
 export * from './flex-box';
-export * from './ImageComponent'
+export * from './ImageComponent';
+export * from './PressableComp';
